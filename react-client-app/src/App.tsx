4a09753b4fb60c39a76673e6c6ec432eb9df2492
Lucas-Casariego <casariegolucas77@gmail.com>
2023-01-18 23:19:19 -0300

    End of section 3

---
+++ react-client-app/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios';
+import { IconButton} from '@mui/material';
+import {PeopleAltRounded} from '@mui/icons-material';
+
+function App() {
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, []) // [] means run once (not strictly true)
+
+  return (
+    <div className="App">
+      <PeopleAltRounded  />
+      <h2>Reactivities</h2>
+      <ul>
+        {activities.map((activity: any) => (
+          <li key={activity.id}>
+            {activity.title}
+          </li>
+          ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
